bffcae1ebdfb0ac39da0292c2adf62bbcbef0544
Tommatino <tomowczarski@gmail.com>
2023-06-25 10:33:13 +0200

    add products and orders

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
-function Calculator(form, summary) {
-    this.prices = {
-        products: 0.5,
-        orders: 0.25,
-        package: {
-            basic: 0,
-            professional: 25,
-            premium: 60
-        },
-        accounting: 35,
-        terminal: 5
-    };
-    this.form = {
-        products: form.querySelector("#products"),
-        orders: form.querySelector("#orders"),
-        package: form.querySelector("#package"),
-        accounting: form.querySelector("#accounting"),
-        terminal: form.querySelector("#terminal")
-    };
-    this.summary = {
-        list: summary.querySelector("ul"),
-        items: summary.querySelector("ul").children,
-        total: {
-            container: summary.querySelector("#total-price"),
-            price: summary.querySelector(".total__price")
+const inputNumber = document.querySelectorAll(".form__input")
+const selectPackage = document.querySelector(".select__input")
+const inputCheckboxes = document.querySelectorAll("input[type = checkbox]")
+
+const productList = document.querySelector("li[data-id = products]")
+const productCalc = document.querySelector("li[data-id = products] .item__calc")
+const productPrice = document.querySelector("li[data-id = products] .item__price")
+
+const orderList = document.querySelector("li[data-id = orders]")
+const orderCalc = document.querySelector("li[data-id = orders] .item__calc")
+const orderPrice = document.querySelector("li[data-id = orders] .item__price")
+
+inputNumber.forEach(function (inputNumber){
+    inputNumber.addEventListener("input", handleAddNumber)
+    productCalc.innerText = 0
+    productPrice.innerText = 0
+    orderCalc.innerText = 0
+    orderPrice.innerText = 0
+    function handleAddNumber () {
+        if (inputNumber.id === "products") {
+            productCalc.innerText = inputNumber.value + " * $0.5";
+            productPrice.innerText = "$" + inputNumber.value * 0.5;
+            productList.classList.add("open");
         }
-    };
-}
-
-
-
+        if (inputNumber.id === "orders") {
+            orderCalc.innerText = inputNumber.value + " * $0.5";
+            orderPrice.innerText = "$" + inputNumber.value * 0.5;
+            orderList.classList.add("open");
+        }
+    }
+})
